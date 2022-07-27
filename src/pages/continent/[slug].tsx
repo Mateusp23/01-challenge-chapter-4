f1a6e3f314dabb6818787b0d2429f1b15a5a1c7c
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-07-27 14:28:49 -0300

    criando componente de conteudo de texte pag europe

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,12 +1,23 @@
 import { Flex } from '@chakra-ui/react';
-import { Header } from '../../components/Header/index';
-import { ContinentBanner } from '../../components/ContinentBanner/index';
+import { Header } from '../../components/Header';
+import { ContinentBanner } from '../../components/ContinentBanner';
+import { TextContent } from '../../components/TextContent';
  
 export default function Continent() {
   return (
     <Flex direction="column">
       <Header />
       <ContinentBanner />
+
+      <Flex
+        direction="column"
+        maxW="1160px"
+        mx="auto"
+        mb="10"
+        px="1rem"
+      >
+        <TextContent />
+      </Flex>
     </Flex>
   );
 }
