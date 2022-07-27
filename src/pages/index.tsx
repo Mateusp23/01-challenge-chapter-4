67014bd17f53265ab285a1a1d2d680124304e104
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-07-26 23:30:07 -0300

    criando componente Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-
+import { Flex } from '@chakra-ui/react';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Flex direction="column">
+      <Header />
+    </Flex>
   );
 }
