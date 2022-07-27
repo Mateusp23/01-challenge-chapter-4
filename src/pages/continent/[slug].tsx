9e956b36c2468224cb7044e7cdb981c07b8ad9f6
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-07-27 14:16:43 -0300

    criuando componente de Banner da page Europe

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react';
 import { Header } from '../../components/Header/index';
+import { ContinentBanner } from '../../components/ContinentBanner/index';
  
 export default function Continent() {
   return (
     <Flex direction="column">
       <Header />
+      <ContinentBanner />
     </Flex>
   );
 }
